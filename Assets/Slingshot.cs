571d99520a8fa858243623d26cc0a18743c41cb0
haesooKr <59234874+haesooKr@users.noreply.github.com>
2020-02-08 15:35:44 -0500

    Add onMouseDown function
    New variables: prefabProjectile, launchPos, projectile, aimingMode

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
-    
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter()
     {
@@ -20,5 +28,12 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
